d2217f9d0f7c0d9982107840c60f0fb0c259b517
MightySun <sonja.gurtner@gmail.com>
2019-10-01 13:12:32 +0200

    Saving Progress of Transitions

--- src/jfx/Controller.java
+++ src/jfx/Controller.java
@@ -19,7 +19,7 @@ import model.Transition;
 import java.util.List;
 
 public class Controller {
-    /* important parts of the GUI */
+    // important parts of the GUI
     private final int RADIUS = 25;
     @FXML
     private Label formulaLabel, explanationLabel, counterLabel;
@@ -44,7 +44,7 @@ public class Controller {
     // The core of the project: the Automaton including Formula, States, Transitions and Counter
     private CTL ctl;
 
-    /* Methods for controlling the CTL Tool */
+    // Methods for controlling the CTL Tool
     @FXML
     public void initialize() {
         ctl = new CTL();
@@ -132,7 +132,7 @@ public class Controller {
             gc.fillOval(state.getX(), state.getY(), RADIUS, RADIUS);
             for (Transition transition : state.getTransitions()) {
                 State end = ctl.getState(transition.getEnd());
-                System.out.println(state.getId() + " -> " + end.getId());       //todo remove
+                System.out.println(state.getId() + " -> " + end.getId());       // TODO remove
                 drawPath(state, end);
             }
         }
@@ -150,74 +150,6 @@ public class Controller {
         System.out.println("----------");  // TODO remove
     }
 
-    private void drawPath(State start, State end) {
-        int sx, sy, ex, ey;
-        sx = sy = ex = ey = 0;
-
-        if (start.getId() == end.getId()) {
-
-        } else {
-            switch (start.getId()) {
-                case 0:
-                    switch (end.getId()) {
-                        case 1:
-                            sx = start.getX() + RADIUS - 3;
-                            sy = start.getY() + 5;
-                            ex = end.getX() + 3;
-                            ey = end.getY() + 5;
-                            break;
-                        case 2:
-
-                            break;
-                        case 3:
-
-                            break;
-                        case 4:
-
-                            break;
-                    }
-                    break;
-                case 1:
-                    switch (end.getId()) {
-                        case 0:
-                            sx = start.getX() + 5;
-                            sy = start.getY() + RADIUS - 5;
-                            ex = end.getX() + RADIUS - 5;
-                            ey = end.getY() + RADIUS - 5;
-                            break;
-                        case 2:
-
-                            break;
-                        case 3:
-
-                            break;
-                        case 4:
-
-                            break;
-                    }
-                    break;
-
-                case 2:
-
-                    break;
-
-                case 3:
-
-                    break;
-                case 4:
-
-                    break;
-            }
-
-            gc.strokeLine(sx, sy, ex, ey);
-            drawArrowHead(sx, sy, ex, ey);
-
-        }
-    }
-
-    private void drawArrowHead(int sx, int sy, int ex, int ey) {
-    }
-
     public void markState(MouseEvent mouseEvent) {
         if (justChecked) return;
         for (State state : ctl.getStates()) {
@@ -233,5 +165,181 @@ public class Controller {
                 }
             }
         }
+    }
+
+    private void drawPath(State start, State end) {
+        int sx, sy, ex, ey;
+        sx = sy = ex = ey = 0;
+
+        if (start.getId() == end.getId()) {
+            if (start.getId() == 1 || start.getId() == 2) {
+                sx = start.getX() + RADIUS - 4;
+                sy = start.getY() + 5;
+            } else {
+                if (start.getId() == 0 && !ctl.hasFourStates()) {
+                    sx = start.getX() + 3;
+                    sy = start.getY() - 6;
+                } else {
+                    sx = start.getX() - 6;
+                    sy = start.getY() + 3;
+                }
+            }
+            gc.strokeOval(sx, sy, 10, 10);
+        } else {
+            switch (start.getId()) {
+                case 0:
+                    switch (end.getId()) {
+                        case 1:
+                            sx = start.getX() + RADIUS - 3;
+                            sy = start.getY() + 7;
+                            ex = end.getX() + 3;
+                            ey = ctl.hasFourStates() ? end.getY() + 7 : end.getY() + 4;
+                            break;
+                        case 2:
+
+                            break;
+                        case 3:
+
+                            break;
+                        case 4:
+
+                            break;
+                    }
+                    break;
+                case 1:
+                    switch (end.getId()) {
+                        case 0:
+                            sx = start.getX() + 3;
+                            sy = start.getY() + RADIUS - 7;
+                            ex = end.getX() + RADIUS - 3;
+                            ey = end.getY() + RADIUS - 7;
+                            break;
+                        case 2:
+
+                            break;
+                        case 3:
+
+                            break;
+                        case 4:
+
+                            break;
+                    }
+                    break;
+
+                case 2:
+                    switch (end.getId()) {
+                        case 0:
+
+                            break;
+                        case 1:
+
+                            break;
+                        case 3:
+
+                            break;
+                        case 4:
+
+                            break;
+                    }
+                    break;
+
+                case 3:
+                    switch (end.getId()) {
+                        case 0:
+
+                            break;
+                        case 1:
+
+                            break;
+                        case 2:
+
+                            break;
+                        case 4:
+
+                            break;
+                    }
+
+                    break;
+                case 4:
+                    switch (end.getId()) {
+                        case 0:
+
+                            break;
+                        case 1:
+
+                            break;
+                        case 2:
+
+                            break;
+                        case 3:
+
+                            break;
+                    }
+                    break;
+            }
+
+            gc.strokeLine(sx, sy, ex, ey);
+            drawArrowHead(sx, sy, ex, ey);
+        }
+    }
+
+    private void drawArrowHead(int sx, int sy, int ex, int ey) {
+        //int x1, x2, x3, y1, y2, y3;
+        int x1, x2, y1, y2;
+
+        if (sy == ey) {
+            x1 = x2 = (sx < ex ? ex - 5 : ex + 5);
+            y1 = ey + 5;
+            y2 = ey - 5;
+        } else if (sx == ex) {
+            x1 = ex - 5;
+            x2 = ex + 5;
+            y1 = y2 = (sy < ey ? ey - 5 : ey + 5);
+        } else if (sx < ex) {
+            x1 = ex - 10;
+            x2 = ex - 5;
+            y1 = (sy > ey ? ey + 5 : ey - 5);
+            y2 = (sy > ey ? ey + 10 : ey - 10);
+        } else {
+            x1 = ex + 5;
+            x2 = ex + 10;
+            y1 = (sy > ey ? ey - 10 : ey - 5);
+            y2 = (sy > ey ? ey - 5 : ey - 10);
+        }
+
+        gc.strokeLine(ex, ey, x1, y1);
+        gc.strokeLine(ex, ey, x2, y2);
+
+        /*if (sy == ey) {
+            x1 = ex;
+            x2 = x3 = (sx < ex ? ex - 5 : ex + 5);
+            y1 = ey;
+            y2 = ey - 5;
+            y3 = ey + 5;
+        } else if (sx == ex) {
+            x1 = ex;
+            x2 = ex - 5;
+            x3 = ex + 5;
+            y1 = ey;
+            y2 = y3 = (sy < ey ? ey - 5 : ey + 5);
+        } else if (sx < ex) {
+            x1 = ex;
+            x2 = (sy < ey ? ex - 3 : ex - 6);
+            x3 = (sy < ey ? ex - 6 : ex - 3);
+            y1 = ey;
+            y2 = ey - 3;
+            y3 = ey + 3;
+        } else {
+            x1 = ex;
+            x2 = (sy < ey ? ex + 6 : ex + 3);
+            x3 = (sy < ey ? ex + 3 : ex + 6);
+            y1 = ey;
+            y2 = ey - 3;
+            y3 = ey - 3;
+        }
+
+        double[] x = new double[]{x1, x2, x3};
+        double[] y = new double[]{y1, y2, y3};
+        gc.fillPolygon(x, y, 3); */
     }
 }
