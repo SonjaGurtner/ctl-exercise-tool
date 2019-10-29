3a13892aeceb2303d47cfec91de8654a3e5a9687
MightySun <sonja.gurtner@gmail.com>
2019-10-29 13:27:04 +0100

    Make working deployment setup.

--- src/main/java/MainFX.java
+++ src/main/java/MainFX.java
@@ -4,7 +4,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class MainFX extends Application {
 
     public static void main(String[] args) {
         launch(args);
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        final FXMLLoader loader = new FXMLLoader(getClass().getResource("/jfx/sample.fxml"));
+        final FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
         final Parent root = loader.load();
         primaryStage.setTitle("CTL Exercise Tool");
         primaryStage.setScene(new Scene(root, 600, 400));
--- src/main/resources/Controller.java
+++ src/main/resources/Controller.java
@@ -1,5 +1,3 @@
-package jfx;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
